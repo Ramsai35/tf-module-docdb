16941b47436bae7338fb3cd179cc82223089a2b6
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-25 05:50:24 +0530

    advanced subnets

--- data.tf
+++ data.tf
@@ -1,6 +1,7 @@
-#data "aws_caller_identity" "current" {}
-#
-#data "aws_vpc" "default" {
-#  id = var.default_vpc_id
-#
-#}
+data "aws_ssm_parameter" "DB_ADMIN_USER" {
+  name = "${var.env}.docdb.DB_ADMIN_USER"
+}
+
+data "aws_ssm_parameter" "DB_ADMIN_PASS" {
+  name = "${var.env}.docdb.DB_ADMIN_PASS"
+}
--- main.tf
+++ main.tf
@@ -34,3 +34,21 @@ resource "aws_security_group" "docdb" {
     { Name = "${var.env}-docdb-security-group" }
   )
 }
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier     = "${var.env}-docdb-cluster"
+  engine                 = "docdb"
+  engine_version         = var.engine_version
+  master_username        = data.aws_ssm_parameter.DB_ADMIN_USER.value
+  master_password        = data.aws_ssm_parameter.DB_ADMIN_PASS.value
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_docdb_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.docdb.id]
+#  storage_encrypted      = true
+#  kms_key_id             = data.aws_kms_key.key.arn
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-docdb-cluster" }
+  )
+}
